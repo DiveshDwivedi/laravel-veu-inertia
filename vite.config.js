5fa87fe4f6f8ef0422b4748bbfd6f20fd8867164
Divesh Dwivedi <dwivedidivesh21@gmail.com>
2025-01-31 12:09:16 +0530

    remove unused dependency

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
     plugins: [
@@ -12,6 +11,5 @@ export default defineConfig({
             refresh: true,
         }),
         vueDevTools(),
-        createHtmlPlugin({})
     ],
 });
