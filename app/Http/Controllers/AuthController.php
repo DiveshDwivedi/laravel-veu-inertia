a001f367c9a0536316307b906f14e10b7c8b1bc2
Divesh Dwivedi <dwivedidivesh21@gmail.com>
2025-01-31 17:46:24 +0530

    error handle and useForm

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     function register(Request $request)  {
-      
         // input validate 
         $validated = $request->validate([
             'name' => 'string|required|min:4|max:20',
-            'email' => 'email|required|unique',
-            'password' => 'password|required|min:8|max21'
+            'email' => 'email|required|max:221|unique:users',
+            'password' => 'required|confirmed|min:8|max:21'
         ]);
 
         // Register user 
+        $user = User::create($validated);
 
         // Login user 
-
+        Auth::login($user);
+        
         // Authentication check 
     }
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,8 +1,7 @@
 <script setup>
-    import { reactive } from 'vue';
-    import { router } from '@inertiajs/vue3'
+    import { useForm } from '@inertiajs/vue3'
 
-    const formData = reactive({
+    const formData = useForm({
         name: null,
         email: null,
         password: null,
@@ -10,7 +9,7 @@
     });
 
     const submit = () => {
-        router.post('/register', formData);
+        formData.post('/register');
     }
 
 </script>
@@ -23,21 +22,25 @@
       <div class="mb-6">
         <label for="name">Name : </label>
         <input type="text" v-model="formData.name" class="text-black" />
+         <div v-if="formData.errors.name">{{ formData.errors.name }}</div>
       </div>
       <div class="mb-6">
         <label for="email">Email : </label>
         <input type="text" v-model="formData.email" class="text-black" />
+         <div v-if="formData.errors.email">{{ formData.errors.email }}</div>
       </div>
       <div class="mb-6">
         <label for="password">Password : </label>
         <input type="password" v-model="formData.password" class="text-black" />
+         <div v-if="formData.errors.password">{{ formData.errors.password }}</div>
       </div>
       <div class="mb-6">
         <label for="password_confirmation">Confirm Password : </label>
         <input type="password" v-model="formData.password_confirmation" class="text-black" />
+         <div v-if="formData.errors.password_confirmation">{{ formData.errors.password_confirmation }}</div>
       </div>
       <div>
-        <button class="text-center mx-20 border bottom-2 p-1" type="submit">Submit</button>
+        <button class="text-center mx-20 border bottom-2 p-1" :disabled="formData.processing" type="submit">Submit</button>
       </div>
     </form>
   </div>
