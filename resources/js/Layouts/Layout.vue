12a4dbab5977b2b8b8011cf6df1218d123468749
Divesh Dwivedi <dwivedidivesh21@gmail.com>
2025-02-02 18:50:51 +0530

    feat: dashboard auth guest middleware added

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -6,16 +6,17 @@
         <header class="bg-blue-400 text-white">
             <nav class="flex items-center p-4 justify-between max-w-screen-lg mx-auto">
                <div class="space-x-6">
-                <Link :href="route('home')">Home</Link>
-                <Link :href="route('about')">About</Link>
-                <Link :href="route('contact')">Contact us</Link>
+                <Link :href="route('home')" :class="{'bg-slate-700 p-1' : $page.component == 'Home'}">Home</Link>
+                <Link :href="route('about')" :class="{'bg-slate-700 p-1' : $page.component == 'About'}">About</Link>
+                <Link :href="route('contact')" :class="{'bg-slate-700 p-1' : $page.component == 'Contact'}">Contact us</Link>
                </div>
-               <div v-if="$page.props.auth.user">
+               <div v-if="$page.props.auth.user" class="space-x-6">
+                <Link :href="route('dashboard')" :class="{'bg-slate-700 p-1' : $page.component == 'Auth/Dashboard'}" >Dashboard</Link>
                 <Link :href="route('logout')" method="post" as="button">Logout</Link>
                </div>
                <div v-else class="space-x-6">
-                <Link :href="route('register')">Register</Link>
-                <Link :href="route('login')">Login</Link>
+                <Link :href="route('register')" :class="{'bg-slate-700 p-1' : $page.component == 'Auth/Register'}">Register</Link>
+                <Link :href="route('login')" :class="{'bg-slate-700 p-1' : $page.component == 'Auth/Login'}">Login</Link>
                </div> 
             </nav>
         </header>
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::inertia('/', 'Home')->name('home');
 
-Route::inertia('/contact', 'Contact')->name('contact');
-Route::inertia('/about', 'About')->name('about');
+Route::middleware('guest')->group(function() {
+    Route::inertia('/register', 'Auth/Register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::inertia('/register', 'Auth/Register')->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+    Route::inertia('/login', 'Auth/Login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
-Route::inertia('/login', 'Auth/Login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth')->group(function(){
+    Route::inertia('/dashboard', 'Auth/Dashboard')->name('dashboard');
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::inertia('/', 'Home')->name('home');
+    Route::inertia('/contact', 'Contact')->name('contact');
+    Route::inertia('/about', 'About')->name('about');
+    
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
